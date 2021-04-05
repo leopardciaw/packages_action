2ef99b6250cf9afb34b3893f0d2f88dcfb7211b4
有種 <39034242+kenzok8@users.noreply.github.com>
2021-04-05 16:01:32 +0800

    Update LEDE.sh

--- LEDE.sh
+++ LEDE.sh
@@ -15,20 +15,20 @@ svn co https://github.com/frainzy1477/luci-app-clash/trunk ./luci-app-clash
 rm -rf luci-app-OpenClash
 rm -rf OpenClash
 git clone https://github.com/vernesong/OpenClash.git
-mv -f OpenClash/luci-app-openclash .
+mv -f OpenClash/luci-app-openclash
 rm -rf OpenClash
 svn co https://github.com/garypang13/luci-app-bypass/trunk ./luci-app-bypass
 svn co https://github.com/garypang13/luci-app-eqos/trunk ./luci-app-eqos
 svn co https://github.com/garypang13/luci-app-dnsfilter/trunk ./luci-app-dnsfilter
-svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 ./luci-theme-edge
+svn co https://github.com/garypang13/luci-theme-edge/trunk ./luci-theme-edge
 git clone https://github.com/garypang13/smartdns-le.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./lua-maxminddb
+git clone https://github.com/tty228/luci-app-serverchan.git
+svn co https://github.com/jerrykuku/lua-maxminddb/trunk lua-maxminddb
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
-svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 ./luci-theme-argon
+svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
-svn co https://github.com/tty228/luci-app-serverchan/trunk ./luci-app-serverchan
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping
@@ -46,4 +46,3 @@ rm -rf ./*/.git
 rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
 exit 0
-
